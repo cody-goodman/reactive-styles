c6e254328674d7e5f2e5d51a66c9d64691281b81
Cody Goodman <goodman.c104@gmail.com>
2018-07-22 14:03:51 -0400

    transpiled source code to es5 compatibility

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const Device = require('./Device');
+const StyleSheet = require('./StyleSheet');
+const DeviceGroup = require('./DeviceGroup');
+const DefaultSizes = require('./DefaultSizes');
+const ReactiveMedia = require('./ReactiveMedia');
+
+module.exports = {
+    Device: Device,
+    StyleSheet: StyleSheet,
+    DeviceGroup: DeviceGroup,
+    DefaultSizes: DefaultSizes,
+    ReactiveMedia: ReactiveMedia
+};
