d7154c58be6af07c9a6da77f9e8ae52811475148
Cody Goodman <goodman.c104@gmail.com>
2018-07-22 15:10:21 -0400

    fixed default devices bug

--- src/DefaultSizes.js
+++ src/DefaultSizes.js
@@ -1,26 +1,25 @@
 const Device = require('./Device');
 const DeviceGroup = require('./DeviceGroup');
 
-const DeviceGroups = [
-    new DeviceGroup('mobile', {maxWidth: 749}),
-    new DeviceGroup('tablet', {minWidth: 650, maxWidth: 991}),
-    new DeviceGroup('desktop', {minWidth: 992})
-];
+const mobile = new DeviceGroup('mobile', {maxWidth: 749});
+const tablet = new DeviceGroup('tablet', {minWidth: 650, maxWidth: 991});
+const desktop = new DeviceGroup('desktop', {minWidth: 992});
+const DeviceGroups = [mobile, tablet, desktop];
 
 const Devices = [
-    new Device('galaxyS5', DeviceGroups.mobile, 360, 640),
-    new Device('pixel2', DeviceGroups.mobile, 411, 731),
-    new Device('pixel2XL', DeviceGroups.mobile, 411, 823),
-    new Device('iPhone5', DeviceGroups.mobile, 320, 568),
-    new Device('iPhone6', DeviceGroups.mobile, 375, 667),
-    new Device('iPhone7', DeviceGroups.mobile, 375, 667),
-    new Device('iPhone8', DeviceGroups.mobile, 375, 667),
-    new Device('iPhone6Plus', DeviceGroups.mobile, 414, 736),
-    new Device('iPhone7Plus', DeviceGroups.mobile, 414, 736),
-    new Device('iPhone8Plus', DeviceGroups.mobile, 414, 736),
-    new Device('iPhoneX', DeviceGroups.mobile, 375, 812),
-    new Device('iPad', DeviceGroups.tablet, 768, 1024),
-    new Device('iPadPro', DeviceGroups.tablet, 1024, 1366),
+    new Device('galaxyS5', mobile, 360, 640),
+    new Device('pixel2', mobile, 411, 731),
+    new Device('pixel2XL', mobile, 411, 823),
+    new Device('iPhone5', mobile, 320, 568),
+    new Device('iPhone6', mobile, 375, 667),
+    new Device('iPhone7', mobile, 375, 667),
+    new Device('iPhone8', mobile, 375, 667),
+    new Device('iPhone6Plus', mobile, 414, 736),
+    new Device('iPhone7Plus', mobile, 414, 736),
+    new Device('iPhone8Plus', mobile, 414, 736),
+    new Device('iPhoneX', mobile, 375, 812),
+    new Device('iPad', tablet, 768, 1024),
+    new Device('iPadPro', tablet, 1024, 1366),
 ];
 
 
